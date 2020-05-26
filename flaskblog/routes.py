7b2def72a7c121b182d1336577eff01be80737d0
Raju <gujjalpatiraju@gmail.com>
2020-05-26 20:01:19 +0530

    Added User Account and make user to select his/her profile pic

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,20 +1,22 @@
+import os, secrets
 from flask import  render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
+from PIL import Image
 from flaskblog import app, db, bcrypt
 from flask_login import  login_user, current_user, logout_user, login_required
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Raju Chowdary',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'May 25, 2020'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'fripya',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'May 26, 2020'
     }
 ]
 
@@ -63,9 +65,45 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
-    
 
-@app.route('/account')
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ex =os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ex
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    '''rescaling to 125,125 size , so we wont loose any data'''
+    output_size =(125,125)
+    i =Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form =UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            '''saving the profile pic using the funacyion name, ikada save_picture
+            oka argument istundi kada akkda aa argument ee data ni teskoni filename ichi
+            then split chestundi a/c to user data--> renames_hex--> then save to profile_pics folder in
+            hashable format'''
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        '''ikkada manam databse tho matching chustunam manam em enter chesamo,
+        whether it is colliding with the database data or not ani'''
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your Account has been Updated!','success' )
+        return redirect(url_for('account'))
+        '''The above one is post request, the below one is get request'''
+    elif request.method == 'GET': 
+        #incase POST vadithe its like render kabbati , user data kanipidu
+        form.username.data = current_user.username
+        '''form data fileds lo curent_user fields veyi ani meaning'''
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
